409f9fe526f568582c06d6dcbe9b9f95a6fd3a09
Olatunde Haroun <haroun.ola@gmail.com>
2025-02-10 22:32:56 +0000

    Folder structure and file creation

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    domains: ["https://dummyjson.com/products"],
+  }
 };
 
 export default nextConfig;
