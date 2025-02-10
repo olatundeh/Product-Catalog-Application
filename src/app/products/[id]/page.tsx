be2aa0b06b38e6cdfa06afd03dfb4d21e0eeb60c
Olatunde Haroun <haroun.ola@gmail.com>
2025-02-10 23:46:24 +0000

    Added loadspinner to dynamic route page

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from 'react';
 import ProductDetails from '@/components/ProductDetails';
+import LoadingSpinner from '@/components/Shared/LoadingSpinner';
 
 export default async function ProductPage({ params }: { params: { id: string } }) {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<LoadingSpinner />}>
       <ProductDetails productId={params.id} />
     </Suspense>
   );
